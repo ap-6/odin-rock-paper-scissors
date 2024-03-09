0b1b3d756386a6875d83f8b43d588cfec50d62c0
A P <131835703+s81c8f@users.noreply.github.com>
2024-03-09 14:22:01 -0500

    Play 5 games and output winner

--- js/script.js
+++ js/script.js
@@ -26,6 +26,12 @@ function getPlayerChoice() {
     let playerChoice = prompt("Enter one of the following: rock, paper, scissors.")
     playerChoice = playerChoice.toLowerCase();
 
+    //ensure the input is valid 
+    while (checkPlayerChoice(playerChoice) === false) { 
+        playerChoice = prompt("Invalid input. Try again. \nEnter one of the following: rock, paper, scissors.")
+        playerChoice = playerChoice.toLowerCase();
+    }
+
     return playerChoice;
 }
 
@@ -36,48 +42,56 @@ function checkPlayerChoice(playerChoice) { //ensures player input is valid
         return false;
 }
 
-function playGame (playRoundFunction, computerChoice, playerChoice) {
+function playGame(playRoundFunction) {
     let playerScore = 0;
     let computerScore = 0;
     let roundResult;
     
     //plays 5 rounds and keeps score
-    for (let count = 0; count < 5; count++) {
-        roundResult = playRoundFunction(computerChoice, playerChoice);
+    for (let count = 1; count < 6; count++) {
+        //get choices for round
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+
+        //determine round winner
+        roundResult = playRound(computerChoice, playerChoice);
+
+        //output round results
+        alert(outputRoundResults(computerChoice, playerChoice, roundResult, count));
+
+        //adjust scores
         if (roundResult[4] === "w") //player wins
             playerScore++;
         else if (roundResult[4] === "l") //player loses
             computerScore++;
     }
     
-    //compare scores
-    if (playerScore > computerScore) {
+    //compare final scores and outputs game results
+    return outputGameResults(computerScore, playerScore);
+}
+
+function outputRoundResults(computerChoice, playerChoice, roundResult, roundCount) { //shows details of roun
+    let output = "Round " + roundCount + ": ";
+    output += "\nComputer choice: " + computerChoice;
+    output += "\nPlayer choice: " + playerChoice;
+    output += "\nRound result: " + roundResult;
+
+    return output;
+}
+
+function outputGameResults(computerScore, playerScore) {
+    if (playerScore > computerScore) { //player wins
         return "You win the game! You scored " + playerScore + 
                " and the computer scored " + computerScore + ".";
     }
-    else if (playerScore < computerScore) {
+    else if (playerScore < computerScore) { //computer wins
         return "You lose the game! You scored " + playerScore + 
                " and the computer scored " + computerScore + ".";
     }
-    else
-        return "You tied! You both scored " + playerScore + "."
+    else //tie
+        return "You tied the game! You both scored " + playerScore + "."
 }
 
-//get user input
-let playerChoice = getPlayerChoice();
-//ensure the input is valid
-while (checkPlayerChoice(playerChoice) === false) 
-    playerChoice = getPlayerChoice();
+alert(playGame(playRound));
 
-//get computer input
-let computerChoice = getComputerChoice();
 
-let roundResult = playRound(computerChoice, playerChoice);
-
-let outputComputerChoice = document.getElementById("computerOutput");
-let outputPlayerChoice = document.getElementById("playerOutput");
-let outputRound = document.getElementById("roundOutput");
-
-outputComputerChoice.textContent = "computer choice: " + computerChoice;
-outputPlayerChoice.textContent = "player choice: " + playerChoice;
-outputRound.textContent = "round result: " + roundResult;
