6593f91017cc7933bfa39c7bd11ae565bbef04e6
A P <131835703+s81c8f@users.noreply.github.com>
2024-03-16 12:28:34 -0400

    Create basic interface for single game

--- js/script.js
+++ js/script.js
@@ -1,30 +1,132 @@
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3); //random number between 0-2
-    if (computerChoice === 0)
+    let computerRoll = Math.floor(Math.random() * 3); //random number between 0-2
+    
+    if (computerRoll === 0)
         return "rock";
-    else if (computerChoice === 1)
+    else if (computerRoll === 1)
         return "paper";
     else    
         return "scissors";
 }
 
-function playRound(computerSelection, playerSelection) {
+function getRoundOutcome(computerSelection, playerSelection) {
     //tie
     if (computerSelection === playerSelection) 
-        return "Tie! Both players chose " + playerSelection;
+        return "Tie! Both players chose " + playerSelection + ".";
     //computer wins
     else if ((computerSelection === "rock" && playerSelection === "scissors")
             ||(computerSelection === "paper" && playerSelection === "rock")
             ||(computerSelection === "scissors" && playerSelection === "paper"))
-        return "You lose the round! " + computerSelection + " beats " + 
+        return "You lose the round! " + titleCase(computerSelection) + " beats " + 
                 playerSelection + ".";
     //player wins
     else
-        return "You win the round! " + playerSelection + " beats " + 
-                computerSelection;
+        return "You win the round! " + titleCase(playerSelection) + " beats " + 
+                computerSelection  + ".";
 }
 
-function getPlayerChoice() {
+function getPlayerChoice(target){
+    switch (target.id) {
+        case "rock":
+            return "rock";
+        
+        case "paper":
+            return "paper";
+        
+        case "scissors":
+            return "scissors";
+    }
+}
+
+function playGame() {
+    let shapeChoice = document.querySelector("#shape-list");
+
+    let playerChoice = document.querySelector("#player-choice");
+    let playerScore = document.querySelector("#player-score");
+
+    let computerChoice = document.querySelector("#computer-choice");
+    let computerScore = document.querySelector("#computer-score");
+
+    let roundOutcome = document.querySelector("#round-outcome");
+    let roundCount = document.querySelector("#round-count");
+    let count = 1;
+    roundCount.textContent = count;
+    
+    let gameOutcome = document.querySelector("#game-outcome");
+    let gameRestartBtn = document.createElement("button");
+    gameRestartBtn.textContent = "Restart game";
+    let gameRestart = document.querySelector("#restart-game");
+
+    shapeChoice.addEventListener("click", (event) => {
+        let target = event.target;
+
+        if(target.tagName === "BUTTON") {
+            //active game
+            if (+playerScore.textContent < 5 && +computerScore.textContent < 5) { 
+                playerChoice.textContent = getPlayerChoice(target);
+                computerChoice.textContent = getComputerChoice();
+
+                roundOutcome.textContent = getRoundOutcome(computerChoice.textContent, 
+                                                        playerChoice.textContent);
+            
+                adjustScores(playerScore, computerScore, roundOutcome);
+                roundCount.textContent = ++count;
+            }
+            //end game
+            if (+playerScore.textContent === 5 || +computerScore.textContent === 5) {
+                gameOutcome.textContent = getGameOutcome(playerScore, computerScore);
+                gameRestart.appendChild(gameRestartBtn);
+            }
+                
+        }
+    });
+
+    
+}
+
+function adjustScores(playerScore, computerScore, roundOutcome) {
+    //adjust scores based on roundResult string
+    if (roundOutcome.textContent[4] === "w") {
+        playerScore.textContent = +playerScore.textContent + 1;
+    }
+    else if (roundOutcome.textContent[4] === "l") {
+        computerScore.textContent = +computerScore.textContent + 1;
+    }
+}
+
+function getGameOutcome(playerScore, computerScore) {
+    let playerScoreInt = +playerScore.textContent;
+    let computerScoreInt = +computerScore.textContent;
+
+    if (playerScoreInt > computerScoreInt) { //player wins
+        return "You win the game! You scored " + playerScoreInt + 
+               " and the computer scored " + computerScoreInt + ".";
+    }
+    else if (playerScoreInt < computerScoreInt) { //computer wins
+        return "You lose the game! You scored " + playerScoreInt + 
+               " and the computer scored " + computerScoreInt + ".";
+    }
+    else //tie
+        return "You tied the game! You both scored " + playerScoreInt + "."
+}
+
+function titleCase(text) {
+    return text.substring(0, 1).toUpperCase() + text.substring(1);
+}
+
+playGame();
+
+/* 
+function checkPlayerChoice(playerChoice) { //ensures player input is valid
+    if ((playerChoice === "rock") || 
+        (playerChoice === "paper") || 
+        (playerChoice === "scissors"))
+        return true;
+    else
+        return false;
+}
+
+function getPlayerChoiceOLD() {
     let playerChoice = prompt("Enter one of the following: rock, paper, scissors.")
     playerChoice = playerChoice.toLowerCase();
 
@@ -38,62 +140,28 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
-function checkPlayerChoice(playerChoice) { //ensures player input is valid
-    if ((playerChoice === "rock") || 
-        (playerChoice === "paper") || 
-        (playerChoice === "scissors"))
-        return true;
-    else
-        return false;
-}
 
-function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundResult;
-    
-    //plays 5 rounds and keeps score
-    for (let count = 1; count < 6; count++) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
 
-        roundResult = playRound(computerChoice, playerChoice);
 
-        alert(getRoundResults(computerChoice, playerChoice, 
-                              roundResult, count));
+let shapeChoice = document.querySelector("#shape-list");
+let playerChoice = document.querySelector("#player-choice");
 
-        //adjust scores based on roundResult string
-        if (roundResult[4] === "w") 
-            playerScore++;
-        else if (roundResult[4] === "l")
-            computerScore++;
+shapeChoice.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch (target.id) {
+        case "rock":
+            playerChoice.textContent += "rock";
+            break;
+        
+        case "paper":
+            playerChoice.textContent += "paper";
+            break;
+        
+        case "scissors":
+            playerChoice.textContent += "scissors";
+            break;
     }
+});
 
-    return getGameResults(computerScore, playerScore);
-}
-
-function getRoundResults(computerChoice, playerChoice, roundResult, roundCount) { 
-    let output = "Round " + roundCount + ": ";
-    output += "\nComputer choice: " + computerChoice;
-    output += "\nPlayer choice: " + playerChoice;
-    output += "\nRound result: " + roundResult;
-
-    return output;
-}
-
-function getGameResults(computerScore, playerScore) {
-    if (playerScore > computerScore) { //player wins
-        return "You win the game! You scored " + playerScore + 
-               " and the computer scored " + computerScore + ".";
-    }
-    else if (playerScore < computerScore) { //computer wins
-        return "You lose the game! You scored " + playerScore + 
-               " and the computer scored " + computerScore + ".";
-    }
-    else //tie
-        return "You tied the game! You both scored " + playerScore + "."
-}
-
-alert(playGame());
-
-
+*/
